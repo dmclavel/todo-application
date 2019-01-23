c6ba1958880ea8b3703a443f81d73b8d77e5416e
dmclavel <dmclavel@up.edu.ph>
2019-01-23 23:59:06 +0800

    Revised config file

--- server/server.js
+++ server/server.js
@@ -78,7 +78,7 @@ app.delete('/todos/:id', authenticate, (req, res) => {
     });
 });
 
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', authenticate, (req, res) => {
     const id = req.params.id;
     const body = _.pick(req.body, ['text', 'completed']);
 
@@ -90,7 +90,7 @@ app.patch('/todos/:id', (req, res) => {
             body.completedAt = null;
         }
 
-        Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+        Todo.findOneAndUpdate({ _id: id, _creator: req.user._id }, { $set: body }, { new: true })
             .then(todo => {
                 if (!todo)
                     return res.status(404).send();
